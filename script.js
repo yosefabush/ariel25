956f22f251382dcc3ca7b55a8b8d66e243f2b7d1
naor <naorhaguli@gmail.com>
2016-05-25 17:31:54 +0300

    Registarition Page, Verify user page

---
+++ script.js
@@ -0,0 +1,22 @@
+function othername() {
+    var input = document.getElementById("fname").value;
+
+var id=123;
+      if(id==input){
+ window.location.assign("to.html") ;}
+else
+alert("ma gever?!");
+}
+ function aditionalDetails(){
+
+ var input = document.getElementById("usernmae").value;
+var input2 = document.getElementById("job").value;var val=2;
+
+
+if(val==1){
+ window.location.assign("insertpin.html");}
+else
+window.location.assign("ffirstlonigpage.html");
+
+
+}
